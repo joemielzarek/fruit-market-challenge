8fe5bcb54c7a740bde4aa51c2f6ccbfb9cb7fc9b
Joe Mielzarek <joseph.mielzarek@gmail.com>
2016-05-05 14:06:54 -0500

    second commit, end of challenge time

--- fruitmarket.js
+++ fruitmarket.js
@@ -10,11 +10,11 @@
       // avgPrice: PLACEHOLDER
   };
 
-  var appleBeginPrice = randomCurrency(0.50, 9.99);
-  var orangesBeginPrice = randomCurrency(0.50, 9.99);
-  var bananasBeginPrice = randomCurrency(0.50, 9.99);
+  var appleBeginPrice = randomCurrency(0.50, 11.50);
+  var orangesBeginPrice = randomCurrency(0.50, 11.50);
+  var bananasBeginPrice = randomCurrency(0.50, 11.50);
   // var grapesBeginPrice = randomCurrency(0.50, 9.99);
-  var pearsBeginPrice = randomCurrency(0.50, 9.99);
+  var pearsBeginPrice = randomCurrency(0.50, 11.50);
 
 var fruitArray = [['Apples', appleBeginPrice], ['Oranges', orangesBeginPrice], ['Bananas', bananasBeginPrice], ['Pears', pearsBeginPrice]];
 
@@ -27,28 +27,34 @@ var fruitArray = [['Apples', appleBeginPrice], ['Oranges', orangesBeginPrice], [
     //Price to fluctuate between 1 and 50 cents each interval
     //Price is not allowed to go below a cost of 50 cents, or above the cost of 9 dollars and 99 cents.
 
+
+
+// FUNCTION that flucuates price up and down
 function Market(price) {
   var priceFlux = randomCurrency(-0.50, 0.50);
-  console.log(fruitArray[0][1]);
-// Condition to determine prices within 50 cents
-  if (priceFlux + fruitArray[0][1] < 0.50) {
-    fruitArray[0][1] = 0.50;
-  } else if (priceFlux + fruitArray[0][1] > 9.99) {
-    fruitArray[0][1] = 9.99;
-  } else {
-    fruitArray[0][1] += priceFlux;
+
+
+    for (var i = 0; i < fruitArray.length; i++) {
+      priceFlux.toFixed(2);
+    // Condition to determine prices within 50 cents
+    if (priceFlux + fruitArray[i][1] < 0.50) {
+      fruitArray[i][1] = 0.50;
+    } else if (priceFlux + fruitArray[i][1] > 11.50) {
+      fruitArray[i][1] = 11.50;
+    } else {
+      fruitArray[i][1] += priceFlux;
+    }
+
   }
-  return fruitArray[0][1];
+  console.log('New Price',fruitArray);
+  return $('.appleLive').text('Current price: ' +'$' + (fruitArray[0][1]).toFixed(2)),
+  $('.orangeLive').text('Current price: ' +'$' + (fruitArray[1][1]).toFixed(2)),
+  $('.bananaLive').text('Current price: ' +'$' + (fruitArray[2][1]).toFixed(2)),
+  $('.pearLive').text('Current price: ' +'$' + (fruitArray[3][1]).toFixed(2));
 }
-console.log(fruitArray[0][1]);
-var example = Market(fruitArray);
-console.log(example);
 
-
-
-var intervalID = window.setInterval(Market, 100);
-
-console.log(intervalID);
+//Calls function
+var intervalID = window.setInterval(Market, 5000);
 
 
 // Available to the user is a ‘total cash’ and an inventory display that shows how much of each of the fruits they have purchased. Also in the user display, should be an ‘average purchased price’, which shows, on average, how much money they have spent on a given fruit in their inventory.
@@ -60,14 +66,48 @@ console.log(intervalID);
 // Finally, style the whole experience with CSS!
 
 $(document).ready(function() {
-
+var totalCash = 100
+// Appends starting price to DOM
+$('.applePrice').append().text('Starting price: ' + '$' + appleBeginPrice);
+$('.orangePrice').append().text('Starting price: ' + '$' + orangesBeginPrice);
+$('.bananaPrice').append().text('Starting price: ' + '$' + bananasBeginPrice);
+$('.pearPrice').append().text('Starting price: ' + '$' + pearsBeginPrice);
 
 
 });
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+function fruitStock (arrayOfFruit) {
+
+
+}
+
+
+
+
+
+
+
+
 var fruitInfo = {};
+
+
+
+
+
 
 // Starts Game
 function Initializer(fruitList) {
@@ -81,9 +121,10 @@ function Initializer(fruitList) {
 
 
 
-
+function randomStartCurrency(min, max) {
+    return Number((Math.random() * (max - min) + min).toFixed(2));
+}
 
 function randomCurrency(min, max) {
     return Number((Math.random() * (max - min) + min).toFixed(2));
 }
-var totalCash = 100;
